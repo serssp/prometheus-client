297ebe85b8d565ce9dd49459c2ad09f94d5edb7e
Amir Langer <langera@gmail.com>
2017-04-09 10:11:55 +0300

    Move settable gauge to new export mechanism

--- src/test/groovy/com/outbrain/swinfra/metrics/SettableGaugeTest.groovy
+++ src/test/groovy/com/outbrain/swinfra/metrics/SettableGaugeTest.groovy
@@ -1,23 +1,16 @@
 package com.outbrain.swinfra.metrics
 
-import com.outbrain.swinfra.metrics.Gauge.GaugeBuilder
-import com.outbrain.swinfra.metrics.samples.SampleCreator
-import com.outbrain.swinfra.metrics.samples.StaticLablesSampleCreator
-import io.prometheus.client.Collector
-import io.prometheus.client.Collector.MetricFamilySamples
-import io.prometheus.client.Collector.MetricFamilySamples.Sample
 import spock.lang.Specification
 
-import java.util.function.DoubleSupplier
-
 import static com.outbrain.swinfra.metrics.SettableGauge.SettableGaugeBuilder
-import static io.prometheus.client.Collector.Type.GAUGE
 
 class SettableGaugeTest extends Specification {
 
     private static final String NAME = "NAME"
     private static final String HELP = "HELP"
-    private static final SampleCreator sampleCreator = new StaticLablesSampleCreator([:])
+
+    private final SampleConsumer sampleConsumer = Mock(SampleConsumer)
+
 
     def 'SettableGauge should have the correct value'() {
         final double expectedValue = 239487234
@@ -50,67 +43,50 @@ class SettableGaugeTest extends Specification {
 
     def "SettableGauge should return empty samples when nothing was set"() {
         given:
-            final List<Sample> samples = [new Sample(NAME, [], [], 0d)]
-            final MetricFamilySamples metricFamilySamples = new MetricFamilySamples(NAME, GAUGE, HELP, samples)
 
-        when:
             final SettableGauge settableGauge = new SettableGaugeBuilder(NAME, HELP).build()
-
+        when:
+            settableGauge.forEachSample(sampleConsumer)
         then:
-            settableGauge.getSample(sampleCreator) == metricFamilySamples
+            1 * sampleConsumer.apply(NAME, 0d, [], null, null)
     }
 
     def "SettableGauge with labels should return no samples when nothing was set"() {
         given:
-            final MetricFamilySamples metricFamilySamples = new MetricFamilySamples(NAME, GAUGE, HELP, [])
-
-        when:
             final SettableGauge settableGauge = new SettableGaugeBuilder(NAME, HELP).withLabels("label1", "label2").
                     build()
-
+        when:
+            settableGauge.forEachSample(sampleConsumer)
         then:
-            settableGauge.getSample(sampleCreator) == metricFamilySamples
+            0 * sampleConsumer._
     }
 
     def "SettableGauge should return the correct value after it was set"() {
         given:
-            final List<Sample> samples1 = [new Sample(NAME, [], [], 1d)]
-            final MetricFamilySamples metricFamilySamples1 = new MetricFamilySamples(NAME, GAUGE, HELP, samples1)
-
-            final List<Sample> samples2 = [new Sample(NAME, [], [], 2d)]
-            final MetricFamilySamples metricFamilySamples2 = new MetricFamilySamples(NAME, GAUGE, HELP, samples2)
-
-        when:
             final SettableGauge settableGauge = new SettableGaugeBuilder(NAME, HELP).build()
             settableGauge.set(1d)
-
+        when:
+            settableGauge.forEachSample(sampleConsumer)
         then:
-            settableGauge.getSample(sampleCreator) == metricFamilySamples1
+            1 * sampleConsumer.apply(NAME, 1d, [], null, null)
 
         when:
             settableGauge.set(2d)
-
+            settableGauge.forEachSample(sampleConsumer)
         then:
-            settableGauge.getSample(sampleCreator) == metricFamilySamples2
+            1 * sampleConsumer.apply(NAME, 2d, [], null, null)
+
     }
 
     def "SettableGauge with labels should return the correct value after it was set"() {
         given:
-            final List<Sample> samples = [new Sample(NAME, ["label1"], ["value1"], 2d),
-                                          new Sample(NAME, ["label1"], ["value2"], 2d)]
-            final MetricFamilySamples metricFamilySamples = new MetricFamilySamples(NAME, GAUGE, HELP, samples)
-
-        when:
             final SettableGauge settableGauge = new SettableGaugeBuilder(NAME, HELP).withLabels("label1").build()
-            settableGauge.set(2d, "value1")
+            settableGauge.set(1d, "value1")
             settableGauge.set(2d, "value2")
-
+        when:
+            settableGauge.forEachSample(sampleConsumer)
         then:
-            final MetricFamilySamples actualMetricFamilySamples = settableGauge.getSample(sampleCreator)
-            actualMetricFamilySamples.samples.sort() == metricFamilySamples.samples.sort()
-            actualMetricFamilySamples.name == metricFamilySamples.name
-            actualMetricFamilySamples.help == metricFamilySamples.help
-            actualMetricFamilySamples.type == metricFamilySamples.type
-
+            1 * sampleConsumer.apply(NAME, 1d, ['value1'], null, null)
+            1 * sampleConsumer.apply(NAME, 2d, ['value2'], null, null)
     }
 }
