cfdc28e083acd69ec4cf0b858ecf0a304571c894
Michael Arenzon <misha9090@gmail.com>
2017-05-08 19:31:54 +0300

    adding gauge to protobuf test

--- src/test/groovy/com/outbrain/swinfra/metrics/exporter/protobuf/ProtobufFormatterTest.groovy
+++ src/test/groovy/com/outbrain/swinfra/metrics/exporter/protobuf/ProtobufFormatterTest.groovy
@@ -1,9 +1,7 @@
 package com.outbrain.swinfra.metrics.exporter.protobuf
 
-import com.outbrain.swinfra.metrics.Counter
-import com.outbrain.swinfra.metrics.Histogram
-import com.outbrain.swinfra.metrics.MetricCollector
-import com.outbrain.swinfra.metrics.Summary
+import com.outbrain.swinfra.metrics.*
+import io.prometheus.client.Metrics
 import io.prometheus.client.Metrics.MetricFamily
 import spock.lang.Specification
 import spock.lang.Subject
@@ -17,14 +15,16 @@ class ProtobufFormatterTest extends Specification {
         given:
         ByteArrayOutputStream output = new ByteArrayOutputStream()
         MetricCollector collector = Mock(MetricCollector)
+
+        Gauge gauge = new Gauge.GaugeBuilder('Gauge', 'help').withValueSupplier({ 18d }).build()
         Counter counter = new Counter.CounterBuilder('Counter', 'help').build()
         counter.inc(17)
-        Histogram histogram = new Histogram.HistogramBuilder('Histogram', 'help').withBuckets(10.0d).build()
+        Histogram histogram = new Histogram.HistogramBuilder('Histogram', 'help').withBuckets(10d).build()
         histogram.observe(1)
         Summary summary = new Summary.SummaryBuilder('Summary', 'help').withLabels('label').build()
         summary.observe(19, 'labelValue')
 
-        collector.iterator() >> [counter, histogram, summary].iterator()
+        collector.iterator() >> [counter, gauge, histogram, summary].iterator()
         collector.staticLabels >> ['a': 'b']
 
         formatter = new ProtobufFormatter(collector)
@@ -39,18 +39,19 @@ class ProtobufFormatterTest extends Specification {
         }
 
         then:
-        3 == families.size()
-        ['Counter', 'Summary', 'Histogram'] as Set == families.collect { it.name } as Set
+        4 == families.size()
+        ['Counter', 'Gauge', 'Summary', 'Histogram'] as Set == families.collect { it.name } as Set
         ['help'] as Set == families.collect { it.help } as Set
-        17.0d == first(families, { it.name == 'Counter' }).counter.value
-        10.0d == first(families, { it.name == 'Histogram' }).histogram.getBucket(0).upperBound
-        19.0d == first(families, { it.name == 'Summary' }).summary.getQuantile(0).value
-        ['a': 'b'] == extractLabels(families.find { it.name == 'Counter' })
-        ['label': 'labelValue', 'a': 'b'] == extractLabels(families.find { it.name == 'Summary' })
+        ['a': 'b'] == extractLabels(first(families, { it.name == 'Counter' }))
+        ['label': 'labelValue', 'a': 'b'] == extractLabels(first(families, { it.name == 'Summary' }))
+        17d == first(families, { it.name == 'Counter' }).counter.value
+        18d == first(families, { it.name == 'Gauge' }).gauge.value
+        10d == first(families, { it.name == 'Histogram' }).histogram.getBucket(0).upperBound
+        19d == first(families, { it.name == 'Summary' }).summary.getQuantile(0).value
     }
 
-    def extractLabels(metricFamily) {
-        metricFamily.metricList.first().labelList.collectEntries { [(it.name): it.value] }
+    def extractLabels(Metrics.Metric metric) {
+        metric.labelList.collectEntries { [(it.name): it.value] }
     }
 
     def first(List<MetricFamily> collection, Closure<Boolean> predicate) {
